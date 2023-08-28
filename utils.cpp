afd22ec3d70e47e32cce881d4e30f3b71600aaf1
kznr <clyde0203hy@outlook.com>
2023-08-28 23:52:23 +0800

    初步支持多线程处理

--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,14 @@
 #include "pch.h"
 #include "module/imgprogcess/imgprogcess.h"
+#include <semaphore>
+#include <thread>
+#include <mutex>
+#include "utils.h"
+#include <iostream>
 
-uint8_t* utils_16to8(uint16_t * raw) {
+static Utils utils;
+
+uint8_t* utils_16to8(uint16_t* raw) {
 	return img_16to8(raw);
 };
 
@@ -28,4 +35,69 @@ uint8_t* utils_pseudo_get(uint16_t* raw) {
 		}
 	}
 	return ret;
+}
+
+void start_thread() {
+	utils.start_thread();
+}
+
+void stop_thread() {
+	utils.stop_thread();
+}
+
+void img_process_thread() {
+	while (true) {
+		utils.cs->acquire();
+		utils.amp_ret = utils_16to8(utils.amp_raw);
+		utils.depth_ret = utils_pseudo_get(utils.depth_raw);
+	}
+}
+
+uint8_t* get_ret_img() {
+	return utils.send_img();
+}
+
+void recv_img(uint16_t* amp_raw, uint16_t* depth_raw) {
+	utils.recv_img(amp_raw, depth_raw);
+}
+
+void Utils::start_thread() {
+	this->t1 = new std::jthread{ img_process_thread };
+}
+
+void Utils::stop_thread() {
+	this->t1->request_stop();
+}
+
+void Utils::recv_img(uint16_t* amp_raw, uint16_t* depth_raw) {
+	this->amp_raw = amp_raw;
+	this->depth_raw = depth_raw;
+	this->cs->release(1);
+}
+
+uint8_t* Utils::ret_amp() {
+	return this->amp_ret;
+}
+
+uint8_t* Utils::ret_depth() {
+	return this->depth_ret;
+}
+
+Utils::Utils() {
+	std::cout << "Instance create" << std::endl;
+	this->cs = new std::counting_semaphore<1>{ 0 };
+	this->bs = new std::binary_semaphore{ 0 };
+	this->amp_raw = nullptr;
+	this->depth_raw = nullptr;
+	this->amp_ret = nullptr;
+	this->depth_ret = nullptr;
+	this->t1 = nullptr;
+	this->message = 0;
+}
+
+Utils::~Utils() {
+	delete this->cs;
+	delete this->amp_raw;
+	delete this->depth_raw;
+	this->t1->request_stop();
 }
--- utils.h
+++ utils.h
@@ -3,6 +3,8 @@
 #define UTILS_EXPORTS extern "C" __declspec(dllexport)
 
 #include <cstdint>
+#include <semaphore>
+#include <thread>
 
 UTILS_EXPORTS uint8_t* utils_16to8(uint16_t* raw);
 
@@ -11,3 +13,37 @@ UTILS_EXPORTS uint8_t* utils_pseudo_get(uint16_t* raw);
 UTILS_EXPORTS void utils_pseudo_init();
 
 UTILS_EXPORTS void modify_gain(uint8_t gain);
+
+UTILS_EXPORTS void img_process_thread();
+
+UTILS_EXPORTS void start_thread();
+
+UTILS_EXPORTS uint8_t* get_ret_img();
+
+UTILS_EXPORTS void recv_img(uint16_t* raw);
+
+class Utils
+{
+	public:
+	Utils();
+	~Utils();
+
+	void start_thread();
+
+	void stop_thread();
+
+	void recv_img(uint16_t* amp_raw, uint16_t* depth_raw);
+
+	uint8_t* ret_amp();
+	uint8_t* ret_depth();
+
+	std::counting_semaphore<1> *cs;
+	std::binary_semaphore* bs;
+	uint16_t *amp_raw, *depth_raw;
+	uint8_t *amp_ret, *depth_ret;
+	uint16_t message;
+	
+	std::jthread* t1;
+
+};
+
